aa8a1e41ffb4f84d89a89e8bd8dc77731d9991d8
Maeldeis <maeldeis@gmail.com>
2024-01-19 14:43:24 +0100

    created timeblock and before time to move to the next hour at 1 increment

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,16 +1,8 @@
-// initial content with id currentDay
-$("#currentDay").text(dayjs().format("dddd, D MMM YYYY"));
-
-// calculating time slots from 8am to 5pm
+$document.ready(function() {
+    $("#currentDay").text(dayjs().format("dddd, D MMM YYYY"));
 var beforeTime = dayjs().startOf("day").hour(8);
-
-// Create a string to hold list items
-var listItems = '';
-
 for (var i = 0; i < 7; i++) {
     var timeSlot = beforeTime.format("HH:mm");
-    
-    // Create time block HTML
     var timeBlock =
     `<div class="col-12 time-block">
         <div class="row">
@@ -19,15 +11,8 @@ for (var i = 0; i < 7; i++) {
             <button class="col-2 saveBtn" data-index="${i}"><i class="fas fa-save"></i></button>
         </div>
     </div>`;
-    
-    // appending time block to #planner div
     $("#planner").append(timeBlock);
+    beforeTime = beforeTime.add(1, 'hours');
+}});
 
-    // Add timeSlot to the listItems string
-    listItems += `<li>${timeSlot}</li>`;
 
-    beforeTime = beforeTime.add(1, 'hour'); // Increment time for the next block
-}
-
-// Append the listItems to #timeBlockLi ul after the loop
-$("#timeBlockLi").append(listItems);
