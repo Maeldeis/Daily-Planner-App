52bdc3bc3cad57dc3bdd4963cabd379d4dfeb8c7
Maeldeis <maeldeis@gmail.com>
2024-01-19 13:33:51 +0100

    created var with time slots for the planner

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,19 +1,26 @@
+// Set the initial text content of an element with the id "currentDay"
 $("#currentDay").text(dayjs().format("dddd, D MMM YYYY"));
 
-// wanted to include time in 24-hour format
-var currentTime = dayjs().format("dddd, D MMM YYYY, HH:mm:");
+// Get the current time in 24-hour format and set it to the start of the hour
+var currentTime = dayjs().startOf("hour").format("HH:mm");
 
-currentTime = currentTime.startOf("hour");
+// Display the initial current time in a specific format
+console.log(currentTime);
 
-var beforeTime = moment().startOf("day").add(8, "hours");
+// Calculate time slots for the planner
+var beforeTime = dayjs().startOf("day").add(8, "hours");
 
-var time1 = beforeTime.add(1, "hours");
-time1 = time1.format("h:mm A");
-var time2 = beforeTime.add(2, "hours");
-var time3 = beforeTime.add(3, "hours");
-var time4 = beforeTime.add(4, "hours");
-var time5 = beforeTime.add(5, "hours");
+var time1 = beforeTime.add(1, "hours").format("HH:mm");
+$(".block1").text(time1);
 
+var time2 = beforeTime.add(1, "hours").format("HH:mm");
+$(".block2").text(time2);
 
+var time3 = beforeTime.add(1, "hours").format("HH:mm");
+$(".block3").text(time3);
 
+var time4 = beforeTime.add(1, "hours").format("HH:mm");
+$(".block4").text(time4);
 
+var time5 = beforeTime.add(1, "hours").format("HH:mm");
+$(".block5").text(time5);
