577391795c6d90afefe6c2b37d35a60ca496e1b1
Maeldeis <maeldeis@gmail.com>
2024-01-19 14:11:45 +0100

    calculate time slots with for loop 8 to 5

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,17 @@
+// initial content with id currentDay
 $("#currentDay").text($dayjs().format("dddd, D MMM YYYY"));
+// calculating time slots from 8am to 5pm
+var beforeTime = dayjs().startOf("day").hour(8);
+for (var i = 0; i < 7; i++) {
+    var timeSlot = beforeTime.format("HH:mm");
+    var timeBlock = 
+    `<div class="col-12 time-block">
+        <div class="row">
+            <div class="col-1 hour">${timeSlot}</div>
+            <textarea class="col-8 description" id="text-${i}"></textarea>
+            <button class="col-2 saveBtn" data-index="${i}"><i class="fas fa-save"></i></button>
+        </div>
+    </div>`;
+    $("#planner").append(timeBlock);
+}
+
