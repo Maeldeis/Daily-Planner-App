d15d522d6d783fbbc71040094d146ba575930ead
Maeldeis <maeldeis@gmail.com>
2024-01-19 14:37:10 +0100

    reworked the js file from ul to time blocks

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,17 @@
 // initial content with id currentDay
-$("#currentDay").text($dayjs().format("dddd, D MMM YYYY"));
+$("#currentDay").text(dayjs().format("dddd, D MMM YYYY"));
+
 // calculating time slots from 8am to 5pm
 var beforeTime = dayjs().startOf("day").hour(8);
 
+// Create a string to hold list items
+var listItems = '';
+
 for (var i = 0; i < 7; i++) {
     var timeSlot = beforeTime.format("HH:mm");
-    var timeBlock = 
+    
+    // Create time block HTML
+    var timeBlock =
     `<div class="col-12 time-block">
         <div class="row">
             <div class="col-1 hour">${timeSlot}</div>
@@ -13,7 +19,15 @@ for (var i = 0; i < 7; i++) {
             <button class="col-2 saveBtn" data-index="${i}"><i class="fas fa-save"></i></button>
         </div>
     </div>`;
+    
+    // appending time block to #planner div
     $("#planner").append(timeBlock);
+
+    // Add timeSlot to the listItems string
+    listItems += `<li>${timeSlot}</li>`;
+
+    beforeTime = beforeTime.add(1, 'hour'); // Increment time for the next block
 }
 
-
+// Append the listItems to #timeBlockLi ul after the loop
+$("#timeBlockLi").append(listItems);
