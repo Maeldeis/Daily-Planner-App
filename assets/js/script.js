6cbf7e93f57a7e2a743085007d019f41b5d05d6e
Maeldeis <maeldeis@gmail.com>
2024-01-24 00:26:00 +0100

    aligned loop index with data index to start from 1 to fix retrieving data

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,39 +1,43 @@
 $(document).ready(function() {
-
     $("#currentDay").text(dayjs().format("dddd, D MMM YYYY"));
-var beforeTime = dayjs().startOf("day").hour(8);
-for (var i = 0; i < 11; i++) {
-    var timeSlot = beforeTime.format("HH:mm");
-    var timeBlock =
-    `<div class="col-12 time-block">
-        <div class="row">
-            <div class="col-1 hour">${timeSlot}</div>
-            <textarea class="col-8 description" id="text-${i}"></textarea>
-            <button class="col-2 saveBtn" data-index="${i}"><i class="fas fa-save"></i></button>
-        </div>
-    </div>`;
-    $("#planner").append(timeBlock);
-    beforeTime = beforeTime.add(1, 'hours');
-}});
+    var beforeTime = dayjs().startOf("day").hour(8);
+
+    for (var i = 1; i <= 7; i++) {
+        var timeSlot = beforeTime.format("HH:mm");
+        var timeBlock =
+        `<div class="col-12 time-block">
+            <div class="row">
+                <div class="col-1 hour">${timeSlot}</div>
+                <textarea class="col-8 description" id="text-${i}"></textarea>
+                <button class="col-2 saveBtn" data-index="${i}"><i class="fas fa-save"></i></button>
+            </div>
+        </div>`;
+        $("#planner").append(timeBlock);
+        beforeTime = beforeTime.add(1, 'hours');
+    }
+});
+
 function testTime(targetTime, elementSelector) {
     var currentTime = dayjs().startOf("hour");
+    
     if (currentTime.isAfter(targetTime)) {
         $(elementSelector).addClass("past");
     } else if (currentTime.isBefore(targetTime)) {
         $(elementSelector).addClass("future");
-    } else {
+    } else if (currentTime.isSame(targetTime)) {
         $(elementSelector).addClass("present");
     }
-    }
-    $(".saveBtn").on("click", function () {
-        var index = $(this).data("index");
-        var text = $("#text-" + index).val();
-        localStorage.setItem("task-" + index, text);
-      });
-      for (var i = 1; i <= 8; i++) {
-        var savedTask = localStorage.getItem("task-" + i);
-        if (savedTask) {
-          $("#text-" + i).val(savedTask);
-        }
-      }
+}
 
+$(".saveBtn").on("click", function () {
+    var index = $(this).data("index");
+    var text = $("#text-" + index).val();
+    localStorage.setItem("task-" + index, text);
+});
+
+for (var i = 0; i < 11; i++) {
+    var savedTask = localStorage.getItem("task-" + i);
+    if (savedTask) {
+        $("#text-" + i).val(savedTask);
+    }
+}
